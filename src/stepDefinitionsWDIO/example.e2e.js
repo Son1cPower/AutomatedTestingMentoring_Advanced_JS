41e33d10f3068464beae806e39011b34b853d037
Stanislav Nehrii <Stanislav_Nehrii@epam.com>
2023-04-10 16:16:11 +0300

    test CI

--- src/stepDefinitionsWDIO/example.e2e.js
+++ src/stepDefinitionsWDIO/example.e2e.js
@@ -12,4 +12,13 @@ describe('My Login application', () => {
     })
 })
 
+describe('My Login application - 2', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open()
 
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!')
+        await expect(SecurePage.flashAlert).toBeExisting()
+        await expect(SecurePage.flashAlert).toHaveTextContaining(
+            'You logged into a secure area!')
+    })
+})
