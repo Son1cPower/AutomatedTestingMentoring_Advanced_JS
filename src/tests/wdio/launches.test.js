6b819c7ef517d46d79b61261d954bf397699448a
Stanislav Nehrii <Stanislav_Nehrii@epam.com>
2023-04-30 18:35:45 +0300

    fix test

--- src/tests/wdio/launches.test.js
+++ src/tests/wdio/launches.test.js
@@ -5,48 +5,32 @@ import logger from '../../utils/loggers/logger.config'
 
 describe('Open and check all Launches', () => {
 
-    beforeEach('Login with credentials', async () => {
+    before('Login with credentials', async () => {
         logger.info(`Start - login.page.js`)
         await LoginPage.open()
         await LoginPage.login('superadmin', 'erebus')
     });
 
-    it('Select project by title', async () => {
+
+    beforeEach('Select project by title', async () => {
         logger.info(`Start - sideBar.js`)
         await SideBar.selectProjectByTitle('stanislav_nehrii_personal')
         logger.info(`Open launches.page.js`)
         await SideBar.launches.click()
+    });
 
-        //await browser.pause(2000);
-
+    it('Ckeck total count of Launches', async () => {
         const expectedTotalLaunches = 5
         logger.info(`Start - launches.page.js`)
         await expect(await LaunchesPage.getTotalCountOfSelectors(await LaunchesPage.getAllLaunches)).toEqual(expectedTotalLaunches)
-
-        const expectedLaunchesIDs = ['10', '6', '7', '8', '9',];
-        await expect(await LaunchesPage.getArrayOfLaunchesIDs(await LaunchesPage.getAllLaunches)).toEqual(expectedLaunchesIDs)
-
-
-
-
-
-
-
-
-
-        console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-
-        console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-
     })
 
 
-    // it('total launches', async () => {
-    //     await SideBar.selectProjectByTitle('project_js')
-    //     await SideBar.launches.click()
+
+    it('Ckeck exist Launches IDs', async () => {
+        const expectedLaunchesIDs = ['10', '6', '7', '8', '9',];
+        await expect(await LaunchesPage.getArrayOfLaunchesIDs(await LaunchesPage.getAllLaunches)).toEqual(expectedLaunchesIDs)
+    })
 
 
-    //     await browser.pause(5000);
-    // })
 })
-
