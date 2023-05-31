22641ce09e137f5b2963e8e1646f0c059c8c37e2
Stanislav Nehrii <Stanislav_Nehrii@epam.com>
2023-05-31 13:18:55 +0300

    add api tests

--- src/testsAPI/test.spec.js
+++ src/testsAPI/test.spec.js
@@ -4,11 +4,13 @@ const { sendRequest } = require('../helpers/api.helper');
 describe('API Test Suite', () => {
 
 
-  it('should get() all launches for project', async () => {
+  it('Get list of project launches ', async () => {
     const response = await sendRequest('project_js/launch');
 
     expect(response.status).to.equal(200);
     //expect(response.data[0].id).to.equal(1);
+    console.log(response.data)
+    expect(response.data.page.totalElements).to.equal(5);
   });
 
 
