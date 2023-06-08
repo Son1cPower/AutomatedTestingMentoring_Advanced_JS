9eeeaf51e27da843be100c79914e7e439a3bc8a3
Stanislav Nehrii <Stanislav_Nehrii@epam.com>
2023-06-08 17:52:00 +0300

    add api tests

--- src/tests/playwright/launches.playwright.test.js
+++ src/tests/playwright/launches.playwright.test.js
@@ -3,11 +3,11 @@ const { pages } = require('../../pageobjects');
 const conf = require('../../../configs/conf');
 
 test.describe('Open and check all Launches', () => {
-  test('Ckeck total count of Launches', async ({ page }) => {
-    await pages('login').login(conf.LOGIN, conf.PASSWORD);
-    await pages('launches').sideBar.selectProjectByTitle(conf.PROJECT);
-    await pages('launches').sideBar.launches.click();
-  });
+  // test('Ckeck total count of Launches', async ({ page }) => {
+  //   await pages('login').login(conf.LOGIN, conf.PASSWORD);
+  //   await pages('launches').sideBar.selectProjectByTitle(conf.PROJECT);
+  //   await pages('launches').sideBar.launches.click();
+  // });
   test('Log in', async ({ page }) => {
     await page.goto('http://localhost:8080/ui/#login');
     await page.locator("input[placeholder='Login']").fill('superadmin');
