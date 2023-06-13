b974f35eb2b238d3f65b0d5e7b55abfd9436429b
Stanislav Nehrii <Stanislav_Nehrii@epam.com>
2023-06-13 11:33:18 +0300

    Resolve merge conflicts

--- src/tests/wdio/launches.wdio.test1.js
+++ src/tests/wdio/launches.wdio.test1.js
@@ -5,16 +5,14 @@ describe('Open and check all Launches', () => {
   before('Select project by title', async () => {
     expect(await browser.getUrl()).toContain(conf.PROJECT + '/launches/all');
   });
-
+  it('Check exist Launches IDs', async () => {
+    const expectedLaunchesIDs = ['1', '2', '3', '4', '5'];
+    await expect(await page('launches').getArrayOfLaunchesIDs(await page('launches').getAllLaunches)).toEqual(
+      expectedLaunchesIDs);
+  });
   it('Check total count of Launches', async () => {
     const expectedTotalLaunches = 5;
     await expect(await page('launches').getTotalCountOfSelectors(await page('launches').getAllLaunches)).toEqual(
       expectedTotalLaunches);
-  });
-  it('Check exist Launches IDs', async () => {
-    const expectedLaunchesIDs = ['1', '2', '3', '4', '5'];
-    await expect(await page('launches').getArrayOfLaunchesIDs(await page('launches').getAllLaunches)).toEqual(
-      expectedLaunchesIDs,
-    );
   });
 });
